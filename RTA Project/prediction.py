32b903bc40d4715d1c487f69cf3d032d6f3f7147
suwarna baraskar <78205912+suwarna93@users.noreply.github.com>
2022-03-20 11:24:24 +0530

    Add files via upload

---
+++ RTA Project/prediction.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+pickle_in = open('model.pkl', 'rb') 
+model = pickle.load(pickle_in)
+
+
+def ordinal_encoder(input_val, feats): 
+    feat_val = list(1+np.arange(len(feats)))
+    feat_key = feats
+    feat_dict = dict(zip(feat_key, feat_val))
+    value = feat_dict[input_val]
+    return value
+
+
+def get_prediction(data,model):
+    """
+    Predict the class of a given data point.
+    """
+    return model.predict(data)
+    
